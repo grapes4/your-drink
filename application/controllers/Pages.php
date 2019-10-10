55e3cdb73dd3069790643b5da62529902c4107b8
gurui <gurui@cntcc.cn>
2019-10-10 10:52:04 +0800

    init

---
+++ application/controllers/Pages.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gurui
+ * Date: 2019/10/10
+ * Time: 9:35
+ */
+class Pages extends CI_Controller {
+
+    public function view($page = 'home')
+    {
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+        {
+            // Whoops, we don't have a page for that!
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
